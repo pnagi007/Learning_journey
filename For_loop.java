dfa76d022d03fdfaa8bd57085fc65d4007b9191f
Naga Gayathri <162715712+pnagi007@users.noreply.github.com>
2025-05-29 16:47:47 +0530

    Create For_loop.java

---
+++ For_loop.java
@@ -0,0 +1,15 @@
+package gayathri;
+import java.util.Scanner;
+public class For_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int num=scan.nextInt();
+		for(int i=1;i<=num;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}

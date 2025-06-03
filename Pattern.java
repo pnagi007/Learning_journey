53f22227943b3b8af826bdc6965037bf817261f0
Naga Gayathri <162715712+pnagi007@users.noreply.github.com>
2025-06-03 19:36:50 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+package gayathri;
+
+public class Task {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<5;j++)
+			{
+				if(j<=i)
+				{
+					System.out.print("*");
+				}
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
